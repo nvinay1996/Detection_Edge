ccb13f49a1ce606f1d5079b0e84438777c1ad045
Paul Swartz <paulswartz999@gmail.com>
2017-03-31 18:06:21 -0400

    Add files via upload

--- tb_Top_Level.sv
+++ tb_Top_Level.sv
@@ -6,44 +6,35 @@
 // Version:     1.0  Initial Design Entry
 // Description: Top Level Testbench
 
-`timescale 1ns/10ps
+`timescale 1ns/100ps
 
 module tb_Top_Level(); 				//Not sure what to name this
 	localparam CLK_PERIOD=4;  
-	localparam IMG_WIDTH=602;		//Input image width
-	localparam IMG_HEIGHT=602;		//Input image height
-	localparam WINDOW_WIDTH=5;
-	localparam WINDOW_HEIGHT=5;	
-	localparam INPUT="./docs/input.bmp";
-	localparam OUTPUT="./docs/output.bmp";	
-	localparam HEADER_BYTES=138;
-	
-	typedef struct{
-		char red;
-		char green;
-		char blue;
-		char unused;
-	} RGB;
-	
-	int i;
+	localparam IMG_WIDTH=428;		//Input image width
+	localparam IMG_HEIGHT=428;		//Input image height
+	localparam INPUT="./docs/input_airplane.bmp";
+	localparam OUTPUT="./docs/output_airplane.bmp";	
+
+	reg[7:0] img_arr[IMG_HEIGHT*IMG_WIDTH*3*4];
 	int j;
-	int k;
-	int n;
-	int m;
-	RGB next_read;
+	int i;
 	int in_ptr;
 	int out_ptr;
-	int count;
 	int useless;
-	char temp;
-	
+	reg[7:0] temp;
+	int write_flag;
+	int write_start;	
+	int prev_haddr;
+
 	reg tb_clk;
-	reg tb_n_reset;
-	reg tb_hrdata;
+	reg tb_n_rst;
+	reg [31:0] tb_hrdata;
 	reg tb_hready;
-	reg tb_haddr;
-	reg tb_hwdata;
+	reg [31:0] tb_haddr;
+	reg [31:0] tb_hwdata;
 	reg tb_hwrite;
+	reg tb_stop;
+	reg tb_done;
 	
 	always begin
 		tb_clk=0;
@@ -53,100 +44,95 @@ module tb_Top_Level(); 				//Not sure what to name this
 	end
 	
 	generate 
-		Top_Level DUT(.clk(tb_clk),.n_rst(tb_n_rst),.hrdata(tb_hrdata),.hready(tb_hready),.haddr(tb_haddr),.hwdata(tb_hwdata),.hwrite(tb_hwrite));
+		Top_Level DUT(.stop(tb_stop),.done(tb_done),.clk(tb_clk),.n_rst(tb_n_rst),.hrdata(tb_hrdata),.hready(tb_hready),.haddr(tb_haddr),.hwdata(tb_hwdata),.hwrite(tb_hwrite));
 	endgenerate
 
 	initial begin
 		//INITIALIZE
-		count=0;
-		next_read.unused=0;
-		tb_n_reset=0;
+		tb_n_rst=0;
 		tb_hrdata=0;
 		tb_hready=0;
+		write_flag=0;
+		tb_stop=1;
 		@(posedge tb_clk);
 		#1;
-		tb_n_reset=1;
+		tb_n_rst=1;
+		tb_stop=0;
 		#2;
-		in_ptr=$fopen(INPUT,"r");
+		in_ptr=$fopen(INPUT,"rb");
 		out_ptr=$fopen(OUTPUT,"w");
 		$fseek(out_ptr,0,0);
-
-		//CREATE OUTPUT BMP HEADER
-		for(i=0;i<HEADER_BYTES;i+=1) begin
-			if(i==2)
-				$fwrite(out_ptr,"%d",(IMAGE_HEIGHT-2)*(IMAGE_WIDTH-2)*3+HEADER_BYTES);		//File size (bytes)
-				i+=3;
-			else if(i==18)
-				$fwrite(out_ptr,"%d",IMAGE_WIDTH-2);						//Image width (pixels)
-				i+=3;
-			else if(i==22)
-				$fwrite(out_ptr,"%d",IMAGE_HEIGHT-2);						//Image height (pixels)
-				i+=3;
-			else if(i==34)
-				$fwrite(out_ptr,"%d",(IMAGE_HEIGHT-2)*(IMAGE_WIDTH-2)*3);			//Image size (bytes)
-				i+=3;
-			else begin
-				$fseek(in_ptr,i,0);								//Copy input header byte to output header
-				useless=$fscanf(in_ptr,"%c",temp);
-				$fwrite(out_ptr,"%c",temp);
-			end
+		$fseek(in_ptr,0,0);
+	
+		/*
+		for(i=0;i<138;i+=1)begin
+			useless=$fscanf(in_ptr,"%c",temp);
+			$fwrite(out_ptr,"%c",temp);
 		end
+*/
+		for(i=0;i<IMG_HEIGHT*IMG_WIDTH*3;i+=1)begin
+			//useless=$fprintf(in_ptr,"%c",img_arr[i]);
+			img_arr[i]=$fgetc(in_ptr);
+		end
+		$fclose(in_ptr);
 
-		//READ/WRITE DATA IN CONVENIENT ORDER
-		for(i=0;i<(IMG_HEIGHT-2)/(WINDOW_HEIGHT-2);i+=1) begin 					//This loop increments the vertical window position
-			for(j=0;j<(IMG_WIDTH-2)/(WINDOW_WIDTH-2);j+=1) begin 				//This loop increments the horizontal window position
-				for(k=0;k<WINDOW_HEIGHT;k+=1) begin 					//This loop increments the vertical pixel position 
-					for(n=0;k<WINDOW_WIDTH;n+=1) begin 				//This loop increments the horizontal pixel position
-						$fseek(in_ptr,(n+k*IMG_WIDTH+j*(WINDOW_WIDTH-2)+i*(WINDOW_HEIGHT-2)*IMG_WIDTH)*3+HEADER_BYTES,0); //Set input file pointer to the next appropriate pixel to be read from the file 
-						useless=$fscanf(in_ptr,"%c",next_read.red); 			//Read in the next 24 bits from the input file
-						useless=$fscanf(in_ptr,"%c",next_read.green);
-						useless=$fscanf(in_ptr,"%c",next_read.blue);
-						while(0==tb_haddr||tb_hwrite) begin			//While read is NOT occurring or write IS occurring...
-							if(tb_hwrite) begin				//Check if write is occuring
-								@(posedge tb_clk);			
-								#1;
-								tb_hready=1;				//hready indicates that the data has been written
-								#2;					//Wait extra time before sampling input data		
-								$fwrite(out_ptr,"%c",tb_hwdata[23:16]);		//Write greyscaled data to output file
-								$fwrite(out_ptr,"%c",tb_hwdata[15:8]);
-								$fwrite(out_ptr,"%c",tb_hwdata[7:0]);
-								count+=1;				//This counter counts total pixels written to output file
-							end
-							@(posedge tb_clk);
-							#1;
-							tb_hready=0;
-							#2;
-						end		
-						tb_hrdata=next_read;					//Next data to be sent to the ASIC is put on data bus
-						@(posedge tb_clk);
-						#1;
-						tb_hready=1;						//hready indicates that the data can now be read
-						@(posedge tb_clk);
-						#1;
-						tb_hready=0;	
+		i=0;
+		while(i<(IMG_HEIGHT-2)*(IMG_WIDTH-2)*3)begin
+			if(prev_haddr != tb_haddr) begin
+				prev_haddr=tb_haddr;
+				if(tb_hwrite)begin
+					if (write_flag ==0)begin
+						$info("Write has occurred");
+						write_start=tb_haddr;
 					end
+					write_flag=1;
+					@(posedge tb_clk);
+					#1;
+					tb_hready=1;
+					#2;
+					img_arr[tb_haddr*3]=tb_hrdata[23:16];
+					img_arr[tb_haddr*3+1]=tb_hrdata[15:8];
+					img_arr[tb_haddr*3+2]=tb_hrdata[7:0];
+					i+=1;
 				end
-			end
-		end
-
-		//FINISH WRITING
-		while(count!=(IMAGE_WIDTH-2)*(IMAGE_HEIGHT-2)) begin			//Keep writing data to the output file until all the pixels have been written 
-			if(tb_hwrite) begin						//This block of code is identical to the one a few lines above
-				@(posedge tb_clk);
-				#1;
-				tb_hready=1;
-				#2;
-				$fwrite(out_ptr,"%c",tb_hwdata[23:16]);
-				$fwrite(out_ptr,"%c",tb_hwdata[15:8]);
-				$fwrite(out_ptr,"%c",tb_hwdata[7:0]);
-				count+=1;
+				else begin
+					$info("Read has occurred");
+					@(posedge tb_clk);
+					#1;
+					tb_hready=1;
+					tb_hrdata={8'b0,img_arr[tb_haddr*3],img_arr[tb_haddr*3+1],img_arr[tb_haddr*3+2]};
+				end
 			end
 			@(posedge tb_clk);
 			#1;
 			tb_hready=0;
 			#2;
 		end
-		$fclose(in_ptr);
+		
+		for(j=0;j<IMG_WIDTH;j+=1)begin
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+		end	
+		for(i=0;i<IMG_HEIGHT-2;i+=1)begin
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+			for(j=0;j<IMG_WIDTH-2;j+=1)begin
+				$fwrite(out_ptr,"%c",img_arr[3*(i*IMG_WIDTH+j)+write_start*3]);
+				$fwrite(out_ptr,"%c",img_arr[3*(i*IMG_WIDTH+j)+1+write_start*3]);
+				$fwrite(out_ptr,"%c",img_arr[3*(i*IMG_WIDTH+j)+2+write_start*3]);
+			end
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+		end
+		for(j=0;j<IMG_WIDTH;j+=1)begin
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+			$fwrite(out_ptr,"%c",8'd255);
+		end
+		$info("DONE");
 		$fclose(out_ptr);
 	end
 endmodule
